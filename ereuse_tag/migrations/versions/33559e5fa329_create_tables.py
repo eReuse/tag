88f9dece08993d0914c7d22be86e87b304e129da
Cayo Puigdefabregas <cayo@ribaguifi.com>
2020-10-06 17:00:13 +0200

    adding alembic config env

---
+++ ereuse_tag/migrations/versions/33559e5fa329_create_tables.py
@@ -0,0 +1,24 @@
+"""create tables
+
+Revision ID: 33559e5fa329
+Revises: 
+Create Date: 2020-10-06 16:14:03.239097
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '33559e5fa329'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
